3f61424f627a4a4ce8bfd6b7c3b2a68299e1350e
Swakshwar Ghosh <ghoshswakshwar@gmail.com>
2023-12-13 00:17:17 +0530

    12/12 content added

--- school db/Teacher.py
+++ school db/Teacher.py
@@ -8,17 +8,17 @@ class Teacher:
 class TeacherTable:
     def __init__(self):
         self.teacher_table = []
-    
+
     def __str__(self):
         return f"Table contains data of {len(self.teacher_table)} teachers."
-    
+
     def add_teacher_data(self, teacher_data : Teacher):
         self.teacher_table.append(teacher_data)
         return teacher_data
 
     def get_teacher_data(self, id : int):
         return self.teacher_table[id-1]
-    
+
     def update_teacher_data(self, id : int, **kwargs):
         teacher_ins = self.teacher_table[id-1]
         for key, val in kwargs.items():
@@ -26,10 +26,10 @@ class TeacherTable:
                 teacher_ins.name = val
             elif key == 'subject':
                 teacher_ins.subject = val
-    
+
     def delete_teacher_data(self, id : int):
         self.teacher_table[id-1] = None
-    
+
     def list_of_teacher(self):
         for teacher in self.teacher_table:
             if teacher is not None:
